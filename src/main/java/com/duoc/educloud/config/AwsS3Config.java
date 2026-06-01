bba0300446c206a87629d0e26a2059c7eee7c090
maximilianopalasezze <palasezzemaxi@gmail.com>
2026-06-01 15:38:37 -0400

    Configura AWS S3 con credenciales temporales

--- src/main/java/com/duoc/educloud/config/AwsS3Config.java
+++ src/main/java/com/duoc/educloud/config/AwsS3Config.java
@@ -4,30 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsS3Config {
 
-    @Value("${aws.access.key}")
-    private String accessKey;
-
-    @Value("${aws.secret.key}")
-    private String secretKey;
-
     @Value("${aws.s3.region}")
     private String region;
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credenciales = AwsBasicCredentials.create(accessKey, secretKey);
-
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(credenciales))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
